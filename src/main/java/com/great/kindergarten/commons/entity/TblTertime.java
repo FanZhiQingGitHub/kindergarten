545ecc991758a7c169fe6d5832b32339cc4bd4c3
nobody1235 <1668403913@qq.com>
2020-04-14 20:44:08 +0800

    Merge branch 'master' of https://github.com/FanZhiQingGitHub/kindergarten
    # Conflicts:
    #	src/main/java/com/great/kindergarten/commons/entity/TblStudent.java
    2020-4-14时间21:43

--- src/main/java/com/great/kindergarten/commons/entity/TblTertime.java
+++ src/main/java/com/great/kindergarten/commons/entity/TblTertime.java
@@ -8,74 +8,92 @@ import java.util.Date;
 /**
  * 教师考勤信息表实体类
  */
-public class TblTertime {
-    private Integer tertimeid;
-    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm")
-    @DateTimeFormat(pattern ="yyyy-MM-dd")
-    private Date tertime;
-    private String tertimedate;
-    private String tertimeperiod;
-    private Integer tid;
+public class TblTertime
+{
+	private Integer tertimeid;
+	@JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date tertime;
+	private String tertimedate1;
+	private String tertimedate2;
+	private Integer tid;
 
-    public TblTertime() {
-    }
+	private String tattendbegin;
+	private String tattendover;
 
-    public TblTertime(Integer tertimeid, Date tertime, String tertimedate, String tertimeperiod, Integer tid) {
-        this.tertimeid = tertimeid;
-        this.tertime = tertime;
-        this.tertimedate = tertimedate;
-        this.tertimeperiod = tertimeperiod;
-        this.tid = tid;
-    }
+	public Integer getTertimeid()
+	{
+		return tertimeid;
+	}
 
-    public Integer getTertimeid() {
-        return tertimeid;
-    }
+	public void setTertimeid(Integer tertimeid)
+	{
+		this.tertimeid = tertimeid;
+	}
 
-    public void setTertimeid(Integer tertimeid) {
-        this.tertimeid = tertimeid;
-    }
+	public Date getTertime()
+	{
+		return tertime;
+	}
 
-    public Date getTertime() {
-        return tertime;
-    }
+	public void setTertime(Date tertime)
+	{
+		this.tertime = tertime;
+	}
 
-    public void setTertime(Date tertime) {
-        this.tertime = tertime;
-    }
+	public String getTertimedate1()
+	{
+		return tertimedate1;
+	}
 
-    public String getTertimedate() {
-        return tertimedate;
-    }
+	public void setTertimedate1(String tertimedate1)
+	{
+		this.tertimedate1 = tertimedate1;
+	}
 
-    public void setTertimedate(String tertimedate) {
-        this.tertimedate = tertimedate;
-    }
+	public String getTertimedate2()
+	{
+		return tertimedate2;
+	}
 
-    public String getTertimeperiod() {
-        return tertimeperiod;
-    }
+	public void setTertimedate2(String tertimedate2)
+	{
+		this.tertimedate2 = tertimedate2;
+	}
 
-    public void setTertimeperiod(String tertimeperiod) {
-        this.tertimeperiod = tertimeperiod;
-    }
+	public Integer getTid()
+	{
+		return tid;
+	}
 
-    public Integer getTid() {
-        return tid;
-    }
+	public void setTid(Integer tid)
+	{
+		this.tid = tid;
+	}
 
-    public void setTid(Integer tid) {
-        this.tid = tid;
-    }
+	public String getTattendbegin()
+	{
+		return tattendbegin;
+	}
 
-    @Override
-    public String toString() {
-        return "TblTertime{" +
-                "tertimeid=" + tertimeid +
-                ", tertime=" + tertime +
-                ", tertimedate='" + tertimedate + '\'' +
-                ", tertimeperiod='" + tertimeperiod + '\'' +
-                ", tid=" + tid +
-                '}';
-    }
+	public void setTattendbegin(String tattendbegin)
+	{
+		this.tattendbegin = tattendbegin;
+	}
+
+	public String getTattendover()
+	{
+		return tattendover;
+	}
+
+	public void setTattendover(String tattendover)
+	{
+		this.tattendover = tattendover;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "TblTertime{" + "tertimeid=" + tertimeid + ", tertime=" + tertime + ", tertimedate1='" + tertimedate1 + '\'' + ", tertimedate2='" + tertimedate2 + '\'' + ", tid=" + tid + '}';
+	}
 }
